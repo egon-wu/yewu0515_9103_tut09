de43ed4dbeda6c3e2f61cf67670855e673acbb40
egon-wu <yewu0515@uni.sydney.edu.au>
2025-05-02 17:43:43 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,12 +6,13 @@ function draw() {
   background(220);
   let centerX = width / 2;
   let centerY = height / 2;
-  let radius = 100;
+  let diameter = windowWidth / 5; // 圆形直径为屏幕宽度的1/5
 
   fill(100, 150, 255);
   noStroke();
-  ellipse(centerX, centerY, radius, radius);
+  ellipse(centerX, centerY, diameter, diameter);
 }
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
